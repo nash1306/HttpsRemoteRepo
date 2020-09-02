f1e44625f2e68734a0370cc090ac81ef7dd76713
nash1306 <nash808404@gmail.com>
2020-09-02 23:22:09 +0530

    Prototype and Function

---
+++ functions_and_prototypes.js
@@ -0,0 +1,30 @@
+// functions constructor
+
+function Employee(name,designation,yearOfBirth){
+    this.name=name;
+    this.designation=designation;
+    this.yearOfBirth=yearOfBirth;
+
+}
+
+// creating calculateAge() method to Prototype property
+
+Employee.prototype.calculateAge= function(){
+    console.log('The current age is : '+ ( 2020 - this.yearOfBirth));
+
+}
+console.log(Employee.prototype);
+
+// creating Objects
+let Emp1= new Employee('Alex','Junior Tester', 1985);
+console.log(Emp1);
+Emp1.calculateAge();
+
+let Emp2= new Employee('Dexter','Senior Software Developer', 1980);
+console.log(Emp2);
+Emp2.calculateAge();
+
+let Emp3= new Employee('Annie','Junior HR', 1977);
+console.log(Emp3);
+Emp3.calculateAge();
+
